676deb1e7116c64d157df6c6149db57cc2852f7e
pengwushao <2232967457@qq.com>
2024-07-27 23:05:04 +0800

    first commit

---
+++ VulkanWindowCreate/VulkanWindowCreate.cpp
@@ -0,0 +1,78 @@
+﻿#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vulkan/vulkan.hpp>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window_;
+    vk::Instance instance_;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window_ = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        createInstance();
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window_)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window_);
+
+        glfwTerminate();
+    }
+
+    void createInstance()
+    {
+        vk::ApplicationInfo app_info;
+        app_info.setApiVersion(VK_API_VERSION_1_0);
+        
+        vk::InstanceCreateInfo create_info;
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        create_info.setEnabledExtensionCount(glfwExtensionCount);
+        create_info.setPpEnabledExtensionNames(glfwExtensions);
+
+        create_info.setPApplicationInfo(&app_info);
+        instance_ = vk::createInstance(create_info);
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
